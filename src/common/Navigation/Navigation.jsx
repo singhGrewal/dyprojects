2c8ad21cd7b2fb2f909f3f96d159fba9d7aa639e
Tejveer <tejveer.pro@gmail.com>
2017-07-11 16:25:21 -0700

    NAV BAR

--- src/common/Navigation/Navigation.jsx
+++ src/common/Navigation/Navigation.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Navigation = () => (
-  <ul>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/posts">Posts</Link></li>
-    <li><Link to="/resume">Resume</Link></li>
-    <li><Link to="/todoApp">Todo App</Link></li>
-  </ul>
+    <nav className="navbar navbar-default navbar-inverse" role="navigation">
+      <div className="col-xs-12 col-sm-offset-4" id="navfluid">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navigationbar">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"/>
+            <span className="icon-bar"/>
+            <span className="icon-bar"/>
+          </button>
+          <a className="navbar-brand"><Link to="/">Grewal</Link></a>
+        </div>
+        <div className="collapse navbar-collapse" id="navigationbar">
+          <ul className="nav navbar-nav">
+            <li><Link to="/posts">Posts</Link></li>
+            <li><Link to="/resume">Resume</Link></li>
+            <li><Link to="/todoApp">Todo App</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 );
 
 export default Navigation;
